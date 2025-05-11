8a73302a84b0886dddcf3d8a1a2cc17011f2e1a4
Aditya <adityajungshah@gmail.com>
2025-05-11 23:59:23 +0545

    Just a try

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 use endoflife::rust;
 
 use serde_json;
@@ -18,3 +19,18 @@ fn main() {
     json_object
     );
 }
+=======
+fn main() {
+    let mut x = 300;
+    let mut even_numbers = Vec::new(); 
+    while x > 0 {
+        if x % 2 == 0 {
+            println!("{x}");
+            even_numbers.push(x); 
+        }
+        x -= 1;
+    }
+
+    println!("Even numbers: {:?}", even_numbers);
+}
+>>>>>>> 603bfc9 (Just a try)
